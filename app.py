8bc62139707a8a8a381c74e89dec559328eae62e
Vijaya Kumara T K <vijayakumaratk@Vijayas-MacBook-Pro.local>
2025-12-08 19:32:36 +0000

    Code refactor

--- app.py
+++ app.py
@@ -24,18 +24,51 @@ day_lookup = {
     6: "Friday"
 }
 
-date = input ("Enter a date (DD/MM/YYYY): ")
-day, month, year = map(int, date.split('-'))
+def get_date_info():
+    date = input ("Enter a date (DD/MM/YYYY): ")
+    day, month, year = map(int, date.split('/'))
 
-if month in [1, 2]:
-    year -= 1
+    if month in [1, 2]:
+        year -= 1
+    return day, month, year
 
-q = int(day)
-m = int(month_lookup[month])
-k = int(year % 100)
-j = int(year // 100)
+def validate_date(day, month, year):
+    if month < 1 or month > 12:
+        return False
+    
+    if day < 1 or day > 31:
+        return False
+    
+    if month == 2:
+        if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+            if day > 29:
+                return False
+        else:
+            if day > 28:
+                return False
+            
+    if month in [4, 6, 9, 11] and day > 30:
+        return False
+    return True
 
-day_of_week = q + ((13 * (m + 1)) // 5) + k + (k // 4) + (j // 4) + (5 * j)
+def calculate_day_of_week(day, month, year):
+    q = int(day)
+    m = int(month_lookup[month])
+    k = int(year % 100)
+    j = int(year // 100)
+    day_of_week = q + ((13 * (m + 1)) // 5) + k + (k // 4) + (j // 4) + (5 * j)
+    return day_of_week
 
-print("The day of the week is:", day_lookup[day_of_week % 7])
+def print_day_of_week(day_of_week):
+    print("The day of the week is:", day_lookup[day_of_week % 7])
+
+
+if __name__ == "__main__":
+    day, month, year = get_date_info()
+    
+    if not validate_date(day, month, year):
+        print("Invalid date. Please enter a valid date in DD/MM/YYYY format.")
+    else:
+        day_of_week = calculate_day_of_week(day, month, year)
+        print_day_of_week(day_of_week)
 
